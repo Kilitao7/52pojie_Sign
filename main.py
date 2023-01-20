745ae225839acb27a76fbfc11702fe4abaccd507
Kilitao7 <62698689+Kilitao7@users.noreply.github.com>
2023-01-20 14:33:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def sign(cookie):
 def main():
     b = os.environ['POJIE']
     cookie = b
-    print(cookie)
+    cookie = "wzws_sessionid=oGPKMLOAMzYuMTQyLjEzNS4xMzCBYjM2YmFhgmRiMWNhYQ==; htVC_2132_saltkey=a1Qz93m3; htVC_2132_lastvisit=1674191507; htVC_2132_seccodecSwQE=6751129.76c3a2cab861bd0991; htVC_2132_ulastactivity=1674195128%7C0; htVC_2132_auth=32fc9rt%2B%2BHcRRgnEMm6du4yyp8pODHs4iqOuUHwkZ9T0Ok41Galcu3Q7a653Ph2DDzmLmxV3ylcIyIs9gnsF8qz2%2Fn0b; htVC_2132_lip=36.142.135.130%2C1674195128; htVC_2132_connect_is_bind=1; htVC_2132_seccodecS=6751151.59b1d7442c1765f01a; htVC_2132_sid=0; htVC_2132_nofavfid=1; htVC_2132_noticonf=1148795D1D3_3_1; htVC_2132_ttask=1148795%7C20230120; wzws_sid=2458fee8016af69dc60a58f2af20370b279a8bb2f1888ea85f42c39a2d5ee7a9570583319d2814db0f9522a42e3435aa7e9b108513a2215d7fc67fda525e8b14768767d29fb89c16b416c1b7ad084e26b473795d500055c04d6508fe28843733; htVC_2132_checkpm=1; htVC_2132_lastcheckfeed=1148795%7C1674196302; htVC_2132_checkfollow=1; htVC_2132_lastact=1674196311%09home.php%09task"
     sign_msg = sign(cookie=cookie)
     print(sign_msg)
 
